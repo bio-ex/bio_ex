a1d0d7bd8bdbafe2b02bc0e7e64d248e0251be4a
Philip Capel <pcapel@users.noreply.github.com>
2022-11-21 17:43:25 -0600

    Restructure for polymer concept as top level
    The idea of the polymers is that each sub-module represents a
    distinctive type which can then house the relative struct for that
    polymer.
    
    Looking at the implementation for DNA should make this clear. But in
    general, I think that it makes sense to have the struct point to a
    specific `polymer` map that can then be used to define the relevant
    information for a more nuanced piece of data.
    
    Wherever possible (which should be most places), there will be a
    function for building the complex data from a simpler type. As with the
    `Bio.Polymer.Dna.from_binary/2` function.
    
    Additionally, anything in the module that can be defined to act on the
    naive data type (binary) should be able to do so, in addition to
    overloading for the case of the more complicated struct.
    
    The goal is ultimatley to allow users the greatest degree of freedom in
    deciding how they use the system. We won't force them to convert things
    that don't need to be converted unless the operation actually requires
    it.

--- lib/polymer/dna/mappings.ex
+++ lib/polymer/dna/mappings.ex
@@ -1,4 +1,4 @@
-defmodule Bio.Sequence.Mapping do
+defmodule Bio.Polymer.Dna.Mappings do
   @moduledoc """
   Mappings for various sequences.
 
@@ -24,7 +24,7 @@ defmodule Bio.Sequence.Mapping do
 
   ## Example
 
-      iex> Map.get(Bio.Sequence.Mapping.nucleotide_to_name, "a")
+      iex> Map.get(Bio.Polymer.Dna.Mappings.nucleotide_to_name, "a")
       "adenine"
   """
   def nucleotide_to_name do
@@ -42,7 +42,7 @@ defmodule Bio.Sequence.Mapping do
 
   ## Example
 
-      iex> Map.get(Bio.Sequence.Mapping.dna_complement, "a")
+      iex> Map.get(Bio.Polymer.Dna.Mappings.dna_complement, "a")
       "t"
 
   """
@@ -60,7 +60,7 @@ defmodule Bio.Sequence.Mapping do
 
   ## Example
 
-      iex> Map.get(Bio.Sequence.Mapping.rna_complement, "u")
+      iex> Map.get(Bio.Polymer.Dna.Mappings.rna_complement, "u")
       "a"
 
   """
@@ -79,7 +79,7 @@ defmodule Bio.Sequence.Mapping do
 
   ## Example
 
-      iex> Map.get(Bio.Sequence.Mapping.dna_ambiguous, "b")
+      iex> Map.get(Bio.Polymer.Dna.Mappings.dna_ambiguous, "b")
       ["c", "g", "t"]
 
   """
