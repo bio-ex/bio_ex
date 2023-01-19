4409e02ca2027510528259b984df7d85778d36c3
Philip Capel <pcapel@users.noreply.github.com>
2023-01-18 18:56:33 -0600

    Add Benchee dep

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Bio.MixProject do
 
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:benchee, "~> 1.0", only: :dev}
     ]
   end
 
