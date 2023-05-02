4b9d6a5fbe9228870f9b87adb169a3774825b6f9
Philip Capel <pcapel@users.noreply.github.com>
2023-05-02 15:55:15 -0500

    Add fasta read/write
    Still a little rough around the edges, but now we have read/write for
    FASTA and it plays relatively well with the data structures that I have
    defined.

--- lib/behaviors/sequence.ex
+++ lib/behaviors/sequence.ex
@@ -1,4 +1,5 @@
 defmodule Bio.Behaviors.Sequence do
   @callback new(base :: term(), opts :: keyword()) :: struct :: term()
   @callback converter() :: converter :: module()
+  @callback fasta_line() :: line :: String.t()
 end
