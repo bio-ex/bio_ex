c1b8e57282ece74656976fe620ae9147a0069871
Philip Capel <pcapel@users.noreply.github.com>
2023-05-03 15:10:57 -0500

    Add barebones stuff for sequence

--- lib/behaviours/sequence.ex
+++ lib/behaviours/sequence.ex
@@ -1,5 +1,18 @@
 defmodule Bio.Behaviours.Sequence do
+  @moduledoc """
+  Sequence stuff
+  """
+
+  @doc """
+  builds a new struct
+  """
   @callback new(base :: term(), opts :: keyword()) :: struct :: term()
+  @doc """
+  returns a module
+  """
   @callback converter() :: converter :: module()
+  @doc """
+  defines how to write to a fasta file
+  """
   @callback fasta_line(given :: struct()) :: line :: String.t()
 end
