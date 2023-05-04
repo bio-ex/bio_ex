c8777a440679d93866dcc56c41c480fde5726d16
Philip Capel <pcapel@users.noreply.github.com>
2023-05-04 11:21:19 -0500

    Don't have empty tests

--- test/enum_test.exs
+++ test/enum_test.exs
@@ -69,107 +69,107 @@ defmodule BioEnumTest do
              ]
     end
 
-    test "Enum.chunk_every/4" do
-    end
-
-    test "Enum.chunk_while/4" do
-    end
-
-    test "Enum.concat/1" do
-    end
-
-    test "Enum.concat/2" do
-    end
-
-    test "Enum.count/1" do
-    end
-
-    test "Enum.count/2" do
-    end
-
-    test "Enum.count_until/2" do
-    end
-
-    test "Enum.count_until/3" do
-    end
-
-    test "Enum.dedup/1" do
-    end
-
-    test "Enum.dedup_by/2" do
-    end
-
-    test "Enum.drop/2" do
-    end
-
-    test "Enum.drop_every/2" do
-    end
-
-    test "Enum.drop_while/2" do
-    end
-
-    test "Enum.each/2" do
-    end
-
-    test "Enum.empty?/1" do
-    end
-
-    test "Enum.fetch!/2" do
-    end
-
-    test "Enum.fetch/2" do
-    end
-
-    test "Enum.filter/2" do
-    end
-
-    test "Enum.find/2" do
-    end
-
-    test "Enum.find/3" do
-    end
-
-    test "Enum.find_index/2" do
-    end
-
-    test "Enum.find_value/2" do
-    end
-
-    test "Enum.find_value/3" do
-    end
-
-    test "Enum.flat_map/2" do
-    end
-
-    test "Enum.flat_map_reduce/3" do
-    end
-
-    test "Enum.frequencies/1" do
-    end
-
-    test "Enum.frequencies_by/2" do
-    end
-
-    test "Enum.group_by/2" do
-    end
-
-    test "Enum.group_by/3" do
-    end
-
-    test "Enum.intersperse/2" do
-    end
-
-    test "Enum.into/2" do
-    end
-
-    test "Enum.into/3" do
-    end
-
-    test "Enum.join/1" do
-    end
-
-    test "Enum.join/2" do
-    end
+    #   test "Enum.chunk_every/4" do
+    #   end
+    #
+    #   test "Enum.chunk_while/4" do
+    #   end
+    #
+    #   test "Enum.concat/1" do
+    #   end
+    #
+    #   test "Enum.concat/2" do
+    #   end
+    #
+    #   test "Enum.count/1" do
+    #   end
+    #
+    #   test "Enum.count/2" do
+    #   end
+    #
+    #   test "Enum.count_until/2" do
+    #   end
+    #
+    #   test "Enum.count_until/3" do
+    #   end
+    #
+    #   test "Enum.dedup/1" do
+    #   end
+    #
+    #   test "Enum.dedup_by/2" do
+    #   end
+    #
+    #   test "Enum.drop/2" do
+    #   end
+    #
+    #   test "Enum.drop_every/2" do
+    #   end
+    #
+    #   test "Enum.drop_while/2" do
+    #   end
+    #
+    #   test "Enum.each/2" do
+    #   end
+    #
+    #   test "Enum.empty?/1" do
+    #   end
+    #
+    #   test "Enum.fetch!/2" do
+    #   end
+    #
+    #   test "Enum.fetch/2" do
+    #   end
+    #
+    #   test "Enum.filter/2" do
+    #   end
+    #
+    #   test "Enum.find/2" do
+    #   end
+    #
+    #   test "Enum.find/3" do
+    #   end
+    #
+    #   test "Enum.find_index/2" do
+    #   end
+    #
+    #   test "Enum.find_value/2" do
+    #   end
+    #
+    #   test "Enum.find_value/3" do
+    #   end
+    #
+    #   test "Enum.flat_map/2" do
+    #   end
+    #
+    #   test "Enum.flat_map_reduce/3" do
+    #   end
+    #
+    #   test "Enum.frequencies/1" do
+    #   end
+    #
+    #   test "Enum.frequencies_by/2" do
+    #   end
+    #
+    #   test "Enum.group_by/2" do
+    #   end
+    #
+    #   test "Enum.group_by/3" do
+    #   end
+    #
+    #   test "Enum.intersperse/2" do
+    #   end
+    #
+    #   test "Enum.into/2" do
+    #   end
+    #
+    #   test "Enum.into/3" do
+    #   end
+    #
+    # test "Enum.join/1" do
+    # end
+    #
+    # test "Enum.join/2" do
+    # end
 
     test "Enum.map/2" do
       mapped =
@@ -179,90 +179,90 @@ defmodule BioEnumTest do
       assert mapped == ["a", "b", "c", "d"]
     end
 
-    test "Enum.map_every/3" do
-    end
-
-    test "Enum.map_intersperse/3" do
-    end
-
-    test "Enum.map_join/2" do
-    end
-
-    test "Enum.map_join/3" do
-    end
-
-    test "Enum.map_reduce/3" do
-    end
-
-    test "Enum.max/3" do
-    end
-
-    test "Enum.max_by/2" do
-    end
-
-    test "Enum.max_by/4" do
-    end
-
-    test "Enum.member?/2" do
-    end
-
-    test "Enum.min/3" do
-    end
-
-    test "Enum.min_by/2" do
-    end
-
-    test "Enum.min_by/4" do
-    end
-
-    test "Enum.min_max/1" do
-    end
-
-    test "Enum.min_max/2" do
-    end
-
-    test "Enum.min_max_by/2" do
-    end
-
-    test "Enum.min_max_by/4" do
-    end
-
-    test "Enum.product/1" do
-    end
-
-    test "Enum.random/1" do
-    end
-
-    test "Enum.reduce/2" do
-    end
-
-    test "Enum.reduce/3" do
-    end
-
-    test "Enum.reduce_while/3" do
-    end
-
-    test "Enum.reject/2" do
-    end
-
-    test "Enum.reverse/1" do
-    end
-
-    test "Enum.reverse/2" do
-    end
-
-    test "Enum.reverse_slice/3" do
-    end
-
-    test "Enum.scan/2" do
-    end
-
-    test "Enum.scan/3" do
-    end
-
-    test "Enum.shuffle/1" do
-    end
-
+    # test "Enum.map_every/3" do
+    # end
+    #
+    # test "Enum.map_intersperse/3" do
+    # end
+    #
+    # test "Enum.map_join/2" do
+    # end
+    #
+    # test "Enum.map_join/3" do
+    # end
+    #
+    # test "Enum.map_reduce/3" do
+    # end
+    #
+    # test "Enum.max/3" do
+    # end
+    #
+    # test "Enum.max_by/2" do
+    # end
+    #
+    # test "Enum.max_by/4" do
+    # end
+    #
+    # test "Enum.member?/2" do
+    # end
+    #
+    # test "Enum.min/3" do
+    # end
+    #
+    # test "Enum.min_by/2" do
+    # end
+    #
+    # test "Enum.min_by/4" do
+    # end
+    #
+    # test "Enum.min_max/1" do
+    # end
+    #
+    # test "Enum.min_max/2" do
+    # end
+    #
+    # test "Enum.min_max_by/2" do
+    # end
+    #
+    # test "Enum.min_max_by/4" do
+    # end
+    #
+    # test "Enum.product/1" do
+    # end
+    #
+    # test "Enum.random/1" do
+    # end
+    #
+    # test "Enum.reduce/2" do
+    # end
+    #
+    # test "Enum.reduce/3" do
+    # end
+    #
+    # test "Enum.reduce_while/3" do
+    # end
+    #
+    # test "Enum.reject/2" do
+    # end
+    #
+    # test "Enum.reverse/1" do
+    # end
+    #
+    # test "Enum.reverse/2" do
+    # end
+    #
+    # test "Enum.reverse_slice/3" do
+    # end
+    #
+    # test "Enum.scan/2" do
+    # end
+    #
+    # test "Enum.scan/3" do
+    # end
+    #
+    # test "Enum.shuffle/1" do
+    # end
+    #
     test "Enum.slice/2" do
       slice =
         ConsolidatedSequence.new("something wicked this way comes")
@@ -283,76 +283,76 @@ defmodule BioEnumTest do
       assert slide == ["b", "c", "d", "a", "e", "f", "g"]
     end
 
-    test "Enum.sort/1" do
-    end
-
-    test "Enum.sort/2" do
-    end
-
-    test "Enum.sort_by/2" do
-    end
-
-    test "Enum.sort_by/3" do
-    end
-
-    test "Enum.split/2" do
-    end
-
-    test "Enum.split_while/2" do
-    end
-
-    test "Enum.split_with/2" do
-    end
-
-    test "Enum.sum/1" do
-    end
-
-    test "Enum.take/2" do
-    end
-
-    test "Enum.take_every/2" do
-    end
-
-    test "Enum.take_random/2" do
-    end
-
-    test "Enum.take_while/2" do
-    end
-
-    test "Enum.to_list/1" do
-    end
-
-    test "Enum.uniq/1" do
-    end
-
-    test "Enum.uniq_by/2" do
-    end
-
-    test "Enum.unzip/1" do
-    end
-
-    test "Enum.with_index/1" do
-    end
-
-    test "Enum.with_index/2" do
-    end
-
-    test "Enum.zip/1" do
-    end
-
-    test "Enum.zip/2" do
-    end
-
-    test "Enum.zip_reduce/3" do
-    end
-
-    test "Enum.zip_reduce/4" do
-    end
-
-    test "Enum.zip_with/2" do
-    end
-
-    test "Enum.zip_with/3" do
-    end
+    # test "Enum.sort/1" do
+    # end
+    #
+    # test "Enum.sort/2" do
+    # end
+    #
+    # test "Enum.sort_by/2" do
+    # end
+    #
+    # test "Enum.sort_by/3" do
+    # end
+    #
+    # test "Enum.split/2" do
+    # end
+    #
+    # test "Enum.split_while/2" do
+    # end
+    #
+    # test "Enum.split_with/2" do
+    # end
+    #
+    # test "Enum.sum/1" do
+    # end
+    #
+    # test "Enum.take/2" do
+    # end
+    #
+    # test "Enum.take_every/2" do
+    # end
+    #
+    # test "Enum.take_random/2" do
+    # end
+    #
+    # test "Enum.take_while/2" do
+    # end
+    #
+    # test "Enum.to_list/1" do
+    # end
+    #
+    # test "Enum.uniq/1" do
+    # end
+    #
+    # test "Enum.uniq_by/2" do
+    # end
+    #
+    # test "Enum.unzip/1" do
+    # end
+    #
+    # test "Enum.with_index/1" do
+    # end
+    #
+    # test "Enum.with_index/2" do
+    # end
+    #
+    # test "Enum.zip/1" do
+    # end
+    #
+    # test "Enum.zip/2" do
+    # end
+    #
+    # test "Enum.zip_reduce/3" do
+    # end
+    #
+    # test "Enum.zip_reduce/4" do
+    # end
+    #
+    # test "Enum.zip_with/2" do
+    # end
+    #
+    # test "Enum.zip_with/3" do
+    # end
   end
 end
